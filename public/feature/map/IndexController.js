86075960819c13cb7052691c5c4c8761152545c2
Peter Johnson <wiz@wiz.co.nz>
2014-05-09 17:10:32 +0700

    map controls

--- public/feature/map/IndexController.js
+++ public/feature/map/IndexController.js
@@ -1,8 +1,39 @@
 
 app.controller( 'MapIndexController', function( $scope, PeliasGeoJsonLayerManager ) {
 
+  // Helper Functions ( For Context Menu )
+  function showCoordinates (e) { alert( e.latlng ); }
+  function centerMap (e) { map.panTo(e.latlng); }
+  function zoomIn (e) { map.zoomIn(); }
+  function zoomOut (e) { map.zoomOut(); }
+
   // Init map
-  var map = L.map( 'map', { zoomControl: false } );
+  var map = L.map( 'map', {
+    zoomControl: false,
+    contextmenu: true,
+    contextmenuWidth: 140,
+    contextmenuItems: [{
+      text: 'Show coordinates',
+      callback: showCoordinates
+    }, {
+      text: 'Center map here',
+      callback: centerMap
+    }, '-', {
+      text: 'Zoom in',
+      icon: 'images/zoom-in.png',
+      callback: zoomIn
+    }, {
+      text: 'Zoom out',
+      icon: 'images/zoom-out.png',
+      callback: zoomOut
+    }]
+  });
+
+  // Controls
+  map.addControl(new L.Control.ZoomMin());
+  L.control.locate().addTo(map);
+  // L.control.fullscreen().addTo(map);
+  L.Marker.include(L.Mixin.ContextMenu);
 
   // Configure map
   map.addLayer( L.tileLayer.provider( 'MapBox.missinglink.i3ook7ke' ) );
