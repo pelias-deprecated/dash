77aad40793c28975ca6883e180345fd8b6d2baa7
Peter Johnson <wiz@wiz.co.nz>
2014-05-12 19:20:27 +0700

    basic autocomplete

---
+++ public/feature/header/IndexController.js
@@ -0,0 +1,52 @@
+// POST /pelias/_suggest?pretty
+// {
+//     "locality-suggest" : {
+//         "text" : "n",
+//         "completion" : {
+//             "field" : "suggest",
+//             "fuzzy" : {
+//                 "fuzziness" : 2
+//             }
+//         }
+//     }
+// }
+
+
+app.controller( 'HeaderIndexController', function( $scope, $http ) {
+
+  $scope.search = '';
+  $scope.results = [];
+
+  $scope.$watch( 'search', function( input ){
+
+    $http({
+      url: 'http://localhost:9200/pelias/_suggest',
+      method: 'POST',
+      data: {
+        'locality-suggest' : {
+          'text' : input,
+          'completion' : {
+            'field' : 'suggest',
+            'fuzzy' : {
+              'fuzziness' : 2
+            }
+          }
+        }
+      },
+      headers: {'Content-Type': 'application/json'}
+    }).success(function (data, status, headers, config) {
+      if( data && data['locality-suggest'] && data['locality-suggest'].length ){
+        $scope.results = data['locality-suggest'][0].options;
+      }
+      else {
+        $scope.results = [];
+      }
+    }).error(function (data, status, headers, config) {
+      $scope.results = [];
+    });
+
+  });
+
+  console.log( 'HeaderIndexController' );
+
+});
